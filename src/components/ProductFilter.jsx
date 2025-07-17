b5c9ab55b3fdc87afe8c88fe724e70bb0b383670
Eric <erichansson.united@gmail.com>
2025-07-16 20:09:03 -0400

    Found the 'details' tag to easily use the collapse function for it and it will have to work for now but I need to learn how create one myself.

--- src/components/ProductFilter.jsx
+++ src/components/ProductFilter.jsx
@@ -1,9 +1,9 @@
 import {useState} from "react"
 
 function ProductFilter(){
-
+    
     return(
-        <div className="w-80 border-1 rounded-sm p-3">
+        <div className="w-80 border-1 rounded-md p-3 m-10">
             <h2 className="">FILTER BY</h2>
             <div>
                 <div>
@@ -23,29 +23,45 @@ function ProductFilter(){
                     
                 </div>
                 <div>
-                    <div className="sr-only">
-                        <span className="">Availability</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
-                        </svg>
+                    <details className="not-sr-only m-1">
+                        <summary className="">Availability</summary>
+                        <div className="flex flex-col">
+                            <div className="m-1">
+                                <input className="mr-2" type="checkbox"/>
+                                <label>In Stock</label>
+                            </div>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
-                        </svg>
-                    </div>
+                            <div className="m-1">
+                                <input className="mr-2" type="checkbox"/>
+                                <label>Limited Stock</label>
+                            </div>
 
-                    <div className="flex flex-col">
-                        <div className="m-1">
-                            <input className="mr-2" type="checkbox"/>
-                            <label>In Stock</label>
                         </div>
+                    </details>
+                </div>
 
-                        <div className="m-1">
-                            <input className="mr-2" type="checkbox"/>
-                            <label>Limited Stock</label>
+                <div>
+                    <details className="not-sr-only m-1">
+                        <summary className="">Brand</summary>
+                        <search className="border-1 rounded-sm mt-2 mb-1"> 
+                            <form>
+                                <input className="w-full" name="fsrch" placeholder="Search" />
+                            </form>
+                        </search>
+
+                        <div className="flex flex-col">
+                            <div className="m-1">
+                                <input className="mr-2" type="checkbox"/>
+                                <label>In Stock</label>
+                            </div>
+
+                            <div className="m-1">
+                                <input className="mr-2" type="checkbox"/>
+                                <label>Limited Stock</label>
+                            </div>
+
                         </div>
-
-                    </div>
+                    </details>
                 </div>
             </div>
         </div>
